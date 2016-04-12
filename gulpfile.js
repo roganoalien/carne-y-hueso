77c130a9ffd01429086e9c352bffb56b5f421f87
roganoalien <roga.noalien@gmail.com>
2016-04-11 19:17:23 -0500

    ajustes sencillos de imágenes y estilo de logo del header

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp'),
-	imagemin = require('imagemin');
+	imagemin = require('imagemin'),
+	cleanCSS = require('gulp-clean-css');
 
 gulp.task('optimize-img', function(){
 	new imagemin()
-			.src('img/not-optimized/*.jpg')
-			.dest('img')
-			.use(imagemin.jpegtran({progressive: true}))
-			.run(function (err, file) {
-				console.log(files[0]);
-			});
+		.src('img/*/*.{jpg,png}')
+		.dest('build/img/')
+		.use(imagemin.jpegtran({progressive: true}))
+		.run(function(err, files){
+			console.log(files[0]);
+		});
 });
 
-gulp.task('default', ['optimize-img']);
+gulp.task('minify-css', function(){
+	//minificar css
+	return gulp.src('css/*.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('default', ['optimize-img', 'minify-css']);
