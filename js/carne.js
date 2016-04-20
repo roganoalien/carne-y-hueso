0f77974ca331eaff2a965a9114350be76971cbd8
roganoalien <rodrigodelacruz.gb@gmail.com>
2016-04-19 22:52:38 -0500

    ajustes de todo, viendo el problema de background attachment fixed con size cover

--- js/carne.js
+++ js/carne.js
@@ -5,7 +5,8 @@ var CARNE = (function() {
 	};
 	var _initEvents = function _initEvents(){
 		// Eventos a iniciar
-		CARNE.loader.init();
+		// CARNE.loader.init();
+		CARNE.nav.init();
 		// CARNE.scroll.init(); ESPERANDO A TENER EL LOADER DEL SLIDESHOW
 	};
 	return {
@@ -19,10 +20,48 @@ CARNE.loader = (function(){
 	//cargador de imágenes del slideshow
 	var _loader = function _loader(){
 		// acción de loader
+		// fakeit
 	};
 	return{
 		init : function init(){
 			_loader();
+		}
+	}
+})();
+CARNE.nav = (function(){
+	var _$header_item,
+		_$bodyContainer,
+		_currentPos,
+		_id,
+		_onScroll,
+		_goto,
+		_$body_container;
+	var _navVars = function _navVars(){
+		_$header_item = $('header a');
+		_$bodyContainer = $('.body-container');
+		_onScroll = false;
+	};
+	var _navEvents = function _navEvents(){
+		_$header_item.on('click', function(event){
+			event.preventDefault();
+			if(!_onScroll){
+				_onScroll = true;
+				_currentPos = _$bodyContainer.scrollTop();
+				_id = $(this).attr('href');
+				_goto = $(_id).offset().top;
+				_$bodyContainer.animate({
+					scrollTop: _goto + _currentPos - 80
+				}, 500, function(){
+					_onScroll = false;
+				});
+				Ps.update(document.getElementById('container'));
+			}
+		});
+	};
+	return{
+		init : function init(){
+			_navVars();
+			_navEvents();
 		}
 	}
 })();
